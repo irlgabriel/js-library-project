c238dfca5e34d15b99b76cbb5b2304e5625c6961
irlgabriel <radu.gabriel131@gmail.com>
2020-09-06 16:38:50 +0300

    added firebase to the project

---
+++ public/firebase.js
@@ -0,0 +1,14 @@
+
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyDGfkgO__SMjKni5qL8G4Pku1XT5khByfs",
+    authDomain: "javascript-book-library.firebaseapp.com",
+    databaseURL: "https://javascript-book-library.firebaseio.com",
+    projectId: "javascript-book-library",
+    storageBucket: "javascript-book-library.appspot.com",
+    messagingSenderId: "517788260684",
+    appId: "1:517788260684:web:2073e5778ecce98401d7e9",
+    measurementId: "G-S8CBW08016"
+  };
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
